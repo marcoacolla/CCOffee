c4054c9f869dc4a08068f3d1095f6f9835cf8fe7
marcoacolla <marcocolla29@gmail.com>
2025-06-16 16:42:00 -0300

    Adding relative pathing

--- main.py
+++ main.py
@@ -1,14 +1,8 @@
 from cardapio import Cardapio
 #from types import SimpleNamespace
+import os
+dirname = os.path.dirname(__file__)
+image_folder = os.path.join(dirname, 'interface_images')
+print(image_folder + "cafeteria(1).png")
 
-print("Pratos Salgados: ")
-for salgado in Cardapio.pratos_salgados.lista:
-    print(salgado.nome)
-print("")
-print("Pratos Doces:")
-for item in Cardapio.pratos_doces.lista:
-    print(item.nome)
-print("")
-print("Bebidas:")
-for bebida in Cardapio.bebidas.lista:
-    print(bebida.nome)
+
