6d457d9bed67d62203bedcef365578c928d197f7
marcoacolla <marcocolla29@gmail.com>
2025-06-16 16:09:37 -0300

    adding full menu

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from cardapio import Cardapio
+#from types import SimpleNamespace
 
-for item in Cardapio.cardapio:
-    acessItem = Cardapio.cardapio[item]
-    print(acessItem.nome)
+print("Pratos Salgados: ")
+for salgado in Cardapio.pratos_salgados.lista:
+    print(salgado.nome)
+print("")
+print("Pratos Doces:")
+for item in Cardapio.pratos_doces.lista:
+    print(item.nome)
+print("")
+print("Bebidas:")
+for bebida in Cardapio.bebidas.lista:
+    print(bebida.nome)
